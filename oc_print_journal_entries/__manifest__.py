e49443b5ab903f3389b43218c244f5d6c9d27921
Waqas Zafar <waqaszafar1215@gmail.com>
2026-04-08 14:54:36 +0500

    push module

---
+++ oc_print_journal_entries/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Print Journal Entries Report in Odoo',
+    'version': '19.0.0.0',
+    'category': 'Accounting',
+    'license': 'OPL-1',
+    'summary': 'Allow to print pdf report of Journal Entries.',
+    'description': """
+    Allow to print pdf report of Journal Entries.
+    journal entry
+    print journal entry 
+    journal entries
+    print journal entry reports
+    account journal entry reports
+    journal reports
+    account entry reports
+    
+""",
+    'author': 'Odoo Circle',
+    'depends': ['base','account'],
+    'data': [
+            'report/report_journal_entries.xml',
+            'report/report_journal_entries_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    "images":["static/description/Banner.gif"],
+}
+
